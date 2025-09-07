52b84c9c0bcb1cf614f2e1f258cc35cc0984f247
Glenn Gore <glenn.g@affinidi.com>
2025-09-07 16:57:32 +0800

    Working simple web server
    Signed-off-by: Glenn Gore <glenn.g@affinidi.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    // Serve files from the "static" directory
+    let routes = warp::fs::dir("assets");
+
+    println!("Serving static files from ./assets on http://0.0.0.0:8080");
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
 }
